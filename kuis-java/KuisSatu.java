11455c78c68bc44b02b7ddc2775c5d2e693a661c
Wahyu Jebredz <wabredz1234@gmail.com>
2024-03-05 13:14:02 +0700

    Update KuisSatu

--- kuis-java/KuisSatu.java
+++ kuis-java/KuisSatu.java
@@ -4,12 +4,17 @@ public class KuisSatu {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        // Input data kedalam array
-        int[] numbers = new int[10];
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print("Bilangan ke - " + (i + 1) + " : ");
-            numbers[i] = in.nextInt();
-        }
+        // // Input data kedalam array
+        // int[] numbers = new int[10];
+
+        // // Mencari bilangan maksimal dan minimal dengan data dinamis (diinputkan)
+        // for (int i = 0; i < numbers.length; i++) {
+        // System.out.print("Bilangan ke - " + (i + 1) + " : ");
+        // numbers[i] = in.nextInt();
+        // }
+
+        // Mencari bilangan maksimal dan minimal dengan data statis
+        int[] numbers = { 11, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
         // Deklarasi nilai maksimal dan minimal
         int max = numbers[0];
