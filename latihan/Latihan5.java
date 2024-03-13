a711bdae5183b08e3a42afc93cbacc14039bdd90
Wahyu Jebredz <wabredz1234@gmail.com>
2024-03-13 16:55:17 +0700

    Latihan 5 (Foreach)

---
+++ latihan/Latihan5.java
@@ -0,0 +1,15 @@
+package latihan;
+
+public class Latihan5 {
+    public static void main(String[] args) {
+        // Array string
+        String[] namaHari = { "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu" };
+
+        // Looping menggunakan foreach
+        System.out.println("Daftar nama hari:");
+        for (String hari : namaHari) {
+            System.out.println(hari);
+        }
+
+    }
+}
