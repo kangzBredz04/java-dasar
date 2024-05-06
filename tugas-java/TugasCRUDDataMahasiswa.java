f8b7ac3d126b5f1846d79641a25dfeb0659e7d28
wahyu <wabredz1234@gmail.com>
2024-05-06 21:34:48 +0700

    feat: Update CRUD program for managing student data

--- tugas-java/TugasCRUDDataMahasiswa.java
+++ tugas-java/TugasCRUDDataMahasiswa.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * InnerTugasCRUDDataMahasiswa
@@ -48,18 +49,68 @@ abstract class mahasiswa {
     public void setUmur(int umur) {
         this.umur = umur;
     }
+
+    abstract void startProgram();
 }
 
-class prosesProgram extends mahasiswa {
+class prosesProgram extends mahasiswa implements CRUD {
+
+    Scanner in = new Scanner(System.in);
+    int pilMenu;
 
     public prosesProgram(String nama, String nim, int umur) {
         super(nama, nim, umur);
+    }
+
+    @Override
+    void startProgram() {
+        while (true) {
+            System.out.println("---------------------------------------");
+            System.out.println(
+                    "1. Mahasiswa Baru\n2. Tampil Data Mahasiswa\n3. Ubah Data Mahasiswa\n4. Hapus Data Mahasiswa\n");
+            System.out.println("---------------------------------------");
+            System.out.print("Pilih : ");
+            pilMenu = in.nextInt();
+
+            if (pilMenu == 1) {
+                create();
+            } else if (pilMenu == 2) {
+                read();
+            } else if (pilMenu == 3) {
+                update();
+            } else if (pilMenu == 4) {
+                delete();
+            }
+        }
+    }
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void read() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'read'");
+    }
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public void delete() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
     }
 
 }
 
 public class TugasCRUDDataMahasiswa {
     public static void main(String[] args) {
-
+        
     }
 }
